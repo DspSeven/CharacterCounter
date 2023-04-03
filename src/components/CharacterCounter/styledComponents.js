fa40bf7cab8843b67e0c02aee5a73c5455bc855d
DspSeven <119715565+DspSeven@users.noreply.github.com>
2023-04-03 09:06:03 +0530

    Create styledComponents.js

---
+++ src/components/CharacterCounter/styledComponents.js
@@ -0,0 +1,22 @@
+import Styled from 'styled-components'
+
+export const CombineContainer = Styled.div`
+display: flex;`
+
+export const BossContainer = Styled.div`
+display: flex;
+flex-direction: column;
+background-color: #ffc533;
+width: 50%;
+height: 100vh;`
+
+export const CharacterContainer = Styled.form`
+display: flex;
+flex-direction: column;
+justify-content: flex-start;
+align-items: center;
+padding-top: 25px;
+line-height: 50px;
+background-color: #0f172a;
+width: 50%;
+height: 100vh;`
